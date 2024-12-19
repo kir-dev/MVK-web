2cbc50d5e0c907e438ab7a6ebe5c1d8f7ba4bd45
Gergő Bujdosó <bujgergo@gmail.com>
2024-12-19 16:20:24 +0100

    updated news

--- src/components/news/NewsCard.tsx
+++ src/components/news/NewsCard.tsx
@@ -3,43 +3,57 @@ import Link from "next/link";
 import Image from "next/image";
 import { urlForImage } from "@/lib/sanity.image";
 import { Card } from "../ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { NewsModal } from "./NewsModal";
 
 export default function NewsCard({ news }: { news: News }) {
   return (
-    <Link href={`/news/${news.slug.current}`}>
-      <Card className="relative flex flex-row items-center justify-center bg-white max-w-xl h-[200px] overflow-hidden border-2">
-        <Image
-          src={
-            news.thumbnail
-              ? urlForImage(news.thumbnail)?.width(200).height(200).url() ?? ""
-              : ""
-          }
-          alt={news.title}
-          className="object-cover h-full"
-          width={200}
-          height={200}
-        />
-        <div className="bg-transparent z-10 relative px-4 text-center text-ellipsis overflow-hidden h-full max-h-full">
-          <h1 className="text-md text-black font-bold my-4 mb-2">
-            {news.title}
-          </h1>
-          {news.excerpt && (
-            <p className="max-h-[40%] text-ellipsis">{news.excerpt}</p>
-          )}
-          <div className="w-full text-start px-4 pb-4 absolute bottom-0 right-0">
-            <p>
-              {news.author}
-              {" - "}
-              {new Date(news._createdAt).toLocaleDateString("hu-HU", {
-                day: "2-digit",
-                month: "short",
-                year: "numeric",
-              })}
-            </p>
-            <p></p>
+    <Dialog>
+      <DialogTrigger>
+        <Card className="relative flex flex-row items-center justify-center bg-white max-w-xl h-[200px] overflow-hidden border-2">
+          <Image
+            src={
+              news.thumbnail
+                ? urlForImage(news.thumbnail)?.width(200).height(200).url() ??
+                  ""
+                : ""
+            }
+            alt={news.title}
+            className="object-cover h-full"
+            width={200}
+            height={200}
+          />
+          <div className="bg-transparent z-10 relative px-4 text-center text-ellipsis overflow-hidden h-full max-h-full">
+            <h1 className="text-md text-black font-bold my-4 mb-2">
+              {news.title}
+            </h1>
+            {news.excerpt && (
+              <p className="max-h-[40%] text-ellipsis">{news.excerpt}</p>
+            )}
+            <div className="w-full text-start px-4 pb-4 absolute bottom-0 right-0">
+              <p>
+                {news.author}
+                {" - "}
+                {new Date(news._createdAt).toLocaleDateString("hu-HU", {
+                  day: "2-digit",
+                  month: "short",
+                  year: "numeric",
+                })}
+              </p>
+              <p></p>
+            </div>
           </div>
-        </div>
-      </Card>
-    </Link>
+        </Card>
+      </DialogTrigger>
+      <NewsModal article={news} />
+    </Dialog>
   );
 }
