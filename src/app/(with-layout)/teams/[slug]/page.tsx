dd06f0181f30e9802ce35cabbcd0412ee12658a0
Görömbey Lilla Zsófia <gorombeylilla@gmail.com>
2025-02-25 15:28:15 +0100

    link icons below title without url text

--- src/app/(with-layout)/teams/[slug]/page.tsx
+++ src/app/(with-layout)/teams/[slug]/page.tsx
@@ -18,16 +18,20 @@ export default async function TeamPage({
   if (!team) return notFound();
   return (
     <main className="flex flex-col flex-1">
-      <div className="p-4 px-40 flex-1">
+      <div className="p-4 px-10 lg:px-40 flex-1">
         <div className="flex flex-row items-center justify-center gap-4">
           <Image
             src={(team.logo ? urlForImage(team.logo)?.url() : "") ?? ""}
             alt={""}
             className="object-cover"
-            height={50}
-            width={50}
+            height={100}
+            width={100}
           />
           <h1 className="text-3xl font-bold">{team.name}</h1>
+        </div>
+        <div className="flex flex-row flex-wrap justify-center gap-4 p-4">
+          {team.Links &&
+            team.Links.map((link) => <LinkCard key={link._key} link={link} />)}
         </div>
 
         <PortableText
@@ -36,10 +40,6 @@ export default async function TeamPage({
         />
       </div>
       {team.races && <RaceCarousel races={team.races} />}
-      <div className="w-screen flex flex-row flex-wrap justify-center gap-4 p-4">
-        {team.Links &&
-          team.Links.map((link) => <LinkCard key={link._key} link={link} />)}
-      </div>
     </main>
   );
 }
--- src/components/teams/LinkCard.tsx
+++ src/components/teams/LinkCard.tsx
@@ -1,19 +1,13 @@
 import { LinkType } from "@/lib/sanity.types";
-import { Card, CardContent, CardHeader } from "../ui/card";
+import { Card } from "../ui/card";
 import Link from "next/link";
 import { CiLink } from "react-icons/ci";
 
 export default function LinkCard({ link }: { link: LinkType }) {
   return (
     <Link href={link.url} target="_blank">
-      <Card className="hover:bg-[#455a64] hover:text-white cursor-pointer">
-        <CardHeader title={link.title}>{link.title}</CardHeader>
-        <CardContent>
-          <div className="flex flex-row gap-2 items-center">
-            <CiLink size={15} />
-            {link.url}
-          </div>
-        </CardContent>
+      <Card className="hover:bg-[#455a64] hover:text-white cursor-pointer flex items-center justify-center p-4">
+        <CiLink size={30} />
       </Card>
     </Link>
   );
