186fe3625970a38fa7b7fbe4d6a164b6b3f2dda9
Gergő Bujdosó <bujgergo@gmail.com>
2025-02-25 15:48:39 +0100

    Mobile optimization

--- src/components/teams/TeamCardSection.tsx
+++ src/components/teams/TeamCardSection.tsx
@@ -2,20 +2,25 @@ export const dynamic = "force-dynamic";
 import { getClient } from "@/lib/sanity.client";
 import { getTeams } from "@/lib/queries/team.queries";
 import TeamCard from "./TeamCard";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
 
 export default async function TeamCardSection() {
   const client = getClient();
   const teams = await getTeams(client);
   if (!teams) return null;
   return (
-    <div className="w-screen h-fit p-10 bg-gray-200">
-      <h1 className="text-3xl">Csapatok</h1>
-      <div className=" overflow-x-scroll">
-        <div className="flex flex-row items-center w-screen gap-4 justify-center no-scrollbar">
-          {teams.map((data, index) => (
-            <TeamCard key={index} team={data} />
-          ))}
-        </div>
+    <div className="w-full h-fit p-10 bg-gray-200">
+      <h1 className="text-3xl mb-4">Csapatok</h1>
+      <div className="flex flex-row flex-wrap items-center gap-4 justify-center">
+        {teams.map((data, index) => (
+          <TeamCard key={index} team={data} />
+        ))}
       </div>
     </div>
   );
